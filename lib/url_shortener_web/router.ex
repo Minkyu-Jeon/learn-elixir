0725e6d9991dc4953ebafb21a390ae2f3922f9ad
paul-jeon <paul@amanda.co.kr>
2020-08-01 10:40:22 +0900

    fisnished endpoint guide

--- lib/url_shortener_web/router.ex
+++ lib/url_shortener_web/router.ex
@@ -7,6 +7,7 @@ defmodule UrlShortenerWeb.Router do
     plug :fetch_flash
     plug :protect_from_forgery
     plug :put_secure_browser_headers
+    plug UrlShortenerWeb.Plugs.Locale, "en"
   end
 
   pipeline :api do
